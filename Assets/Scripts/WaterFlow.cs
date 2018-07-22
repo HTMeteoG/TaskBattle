82e535a0ff1f3dcd526efc489adb48cf6862da89
Hayato <ht2_gr@ybb.ne.jp>
2018-07-22 19:24:58 +0900

    Make WaterFlow
    And testing in WaterLoop

---
+++ Assets/Scripts/WaterFlow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFlow : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 flow;
+    [SerializeField]
+    float buoyancy = 11;
+    [SerializeField]
+    float decelerationRate = 0.5f;
+
+    public void OnTriggerStay(Collider c)
+    {
+        if (c.attachedRigidbody != null)
+        {
+            Vector3 reduceSpd = c.attachedRigidbody.velocity * -decelerationRate;
+            Vector3 force = Vector3.up * buoyancy + reduceSpd + flow;
+            c.attachedRigidbody.AddForce(force, ForceMode.Force);
+        }
+    }
+}
